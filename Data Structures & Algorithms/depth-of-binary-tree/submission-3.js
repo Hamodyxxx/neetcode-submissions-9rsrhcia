07fb2ac68b7548e86ae8ec788d174795218a19d3
Hamed Eslam <98085828+Hamodyxxx@users.noreply.github.com>
2026-04-03 11:31:39 +0200

    Add: depth-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-3.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root, depth = 0) {
+        if (root === null) {
+            return depth;
+        }
+
+        const leftMax = this.maxDepth(root.left, depth + 1);
+        const rightMax = this.maxDepth(root.right, depth + 1)
+
+        return Math.max(leftMax, rightMax)
+    }
+}
