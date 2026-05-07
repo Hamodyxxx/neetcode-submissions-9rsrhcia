3b7420d55c0767589028f2dc39a3d92679b8616d
Hamed Eslam <98085828+Hamodyxxx@users.noreply.github.com>
2026-05-07 09:21:47 +0300

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int ClimbStairs(int n) {   
+        if(n == 1) return 1;
+        if(n == 2) return 2;  
+        int n1 = 1, n2 = 2;
+        int x;
+        for(int i = 3; i <= n; i++) {
+            x = n1 + n2;
+            n1 = n2;
+            n2 = x;
+        }
+        return n2;
+    }
+}
